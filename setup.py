7f3c10f09388dd74919842b1dc61b1aca281d810
Fer Aguirre <kay_op@protonmail.com>
2022-08-08 11:05:56 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Registro Empresas Cuba',
+    version='0.1.0',
+    author='Fer Aguirre',
+    description='Registro Nacional de Sucursales y Agentes de Sociedades Mercantiles Extranjeras en Cuba',
+    python_requires='>=3',
+    license='MIT License',
+    packages=find_packages(),
+)
