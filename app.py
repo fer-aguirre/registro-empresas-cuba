b5b108c6402dea6144ed963cde4b96ba1fb744e0
Fer Aguirre <kay_op@protonmail.com>
2022-08-09 15:27:22 -0500

    Update streamlit app with streamlit-aggrid

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-import streamlit as st
+import numpy as np
 import pandas as pd
 from PIL import Image
+import streamlit as st
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
 
 def main():
     # Open image with PIL
@@ -14,11 +17,34 @@ def main():
                         page_title="Registro Nacional de Sucursales y Agentes de Sociedades Mercantiles Extranjeras en Cuba",
                         page_icon=favicon,  # String, anything supported by st.image, or None.
     )
-    df = pd.read_excel('./data/raw/registro_empresas.xlsx', index_col=None, sheet_name='empresas', 
+    data = pd.read_excel('./data/raw/registro_empresas.xlsx', index_col=None, sheet_name='empresas', 
                         converters={'Fecha de la resolución': str, 
                                     'Fecha Gaceta': str,
                                     'No. de Resolución': str})
-    st.dataframe(df)
+    # st.dataframe(df)
+
+    gb = GridOptionsBuilder.from_dataframe(data)
+    gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+    gb.configure_side_bar() #Add a sidebar
+    gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+    gridOptions = gb.build()
+
+    grid_response = AgGrid(
+        data,
+        gridOptions=gridOptions,
+        data_return_mode='AS_INPUT', 
+        update_mode='MODEL_CHANGED', 
+        fit_columns_on_grid_load=False,
+        theme='blue', #Add theme color to the table
+        enable_enterprise_modules=True,
+        height=800, 
+        width='100%',
+        reload_data=True
+    )
+
+    data = grid_response['data']
+    selected = grid_response['selected_rows'] 
+    df = pd.DataFrame(selected)
 
 
 
